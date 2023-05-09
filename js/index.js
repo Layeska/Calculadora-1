1c7eec58c65ab8360591baf6853771b32f47e44e
Ana Juárez <lisa13suarez@gmail.com>
2023-05-08 23:41:59 -0600

    editando querySelector

--- js/index.js
+++ js/index.js
@@ -1,24 +1,9 @@
-class Calculadora{
-    sumar(num1,num2){
-        return num1 + num2;
-    }
-    restar(num1,num2){
-        return num1 - num2;
-    }
-    dividir(num1,num2){
-        return num1 / num2;
-    }
-    multiplicar(num1,num2){
-        return num1 * num2;
-    }
-}
-
-const pantallaValor = document.getElementsByClassName("datos")
-const pantallaResult = document.getElementsByClassName("resultado");
+const pantallaValor = document.querySelector(".datos")
+const pantallaResult = document.querySelector(".resultado");
 const numero = document.querySelectorAll(".numero");
 const operadores = document.querySelectorAll(".operador");
 
-const display = new Display(pantallaResult,pantallaValor);
+const display = new Display(pantallaValor,pantallaResult);
 
 numero.forEach(boton => {
     boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
